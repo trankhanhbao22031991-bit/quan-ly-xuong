6b37afd33309efe61a280390a25b768b1397554e
trankhanhbao22031991-bit <trankhanhbao22031991@gmail.com>
2026-05-02 01:31:30 +0700

    Update server.js

--- server.js
+++ server.js
@@ -59,5 +59,8 @@ io.on("connection", (socket) => {
 });
 
 server.listen(3000, () => {
-  console.log("http://localhost:3000");
+  const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
